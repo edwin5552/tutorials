8721e54bd95c93becc8dde3dfe553ddbd061e9ca
Edwin <edwin@encome.in>
2024-03-14 17:40:02 +0530

    Create header.php

---
+++ PHP/ticks/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>The Page</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+</head>
+<body>
+	<header class="site-header bg-primary py-4">
+		<h1 class="mb-0 text-center text-white">Header</h1>
+	</header> <!-- #site-header -->
+	<div class="site-page py-5">
