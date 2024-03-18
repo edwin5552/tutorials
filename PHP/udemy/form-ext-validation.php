b8a5c3e5ae6a30c4ecc53e45c9ae974ff2ab519f
Edwin <edwin@encome.in>
2024-03-18 09:13:27 +0530

    Create form-ext-validation.php

---
+++ PHP/udemy/form-ext-validation.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Form External Validation</title>
+	<style>
+		body {
+			font-size: 20px;
+		}
+	</style>
+</head>
+
+<body>
+
+	<form action="form-ext-validation-connect.php" method="post">
+		<input type="text" name="username" placeholder="username">
+		<input type="password" name="password" placeholder="password">
+		<input type="submit" name="submit">
+	</form>
+
+</body>
+
+</html>
