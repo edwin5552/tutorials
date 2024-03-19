02249d5551e2154597732060e701a476059e9ca1
Edwin <edwin@encome.in>
2024-03-19 12:17:15 +0530

    Create node-1.php

---
+++ PHP/web-node/node-1.php
@@ -0,0 +1,27 @@
+<!DOCTYPEhtml>
+	<html lang=" en ">
+
+	<head>
+		<meta charse t=" UTF - 8 ">
+		<meta http ? equiv=" X - UA - Compatible " content=" IE = edge ">
+		<meta name=" viewport " content=" width = device - width, initial - scale = 1 .0">
+		<title> PHP </title>
+	</head>
+
+	<body>
+		<?php
+
+		$firstVar = 'my name';
+
+		echo $firstVar;
+
+		echo '<h3 class="my-code">'.''. $firstVar .''.'</h3>';
+
+		echo ('PHP'), '<br>';
+
+		'<h2> echo (Code) </h2>';
+		
+		?>
+	</body>
+
+	</html>
