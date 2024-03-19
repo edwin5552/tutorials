60faf5198d746f9463459dc4afc203c8a848c358
Edwin <edwin@encome.in>
2024-03-19 12:17:30 +0530

    Create varibales.php

---
+++ PHP/udemy/varibales.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Variables</title>
+	<style>
+		body {
+			font-size: 20px;
+		}
+	</style>
+</head>
+<body>
+
+	<?php
+
+		$name = "John";
+		$number = 100;
+		$location = "Kochi";
+
+		echo $name;
+		echo "<br>";
+		echo $number;
+		echo "<br>";
+		echo $location;
+		echo "<br>";
+		echo "<br>";
+
+		echo $name . " " . $number . " " . $location;
+
+	?>
+	
+</body>
+</html>
