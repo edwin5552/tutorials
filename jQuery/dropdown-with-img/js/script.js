fa4bd28d31a6bcb1837328b301523abe4fdafc56
Edwin <edwin@encome.in>
2024-05-07 11:10:00 +0530

    Update script.js

--- jQuery/dropdown-with-img/js/script.js
+++ jQuery/dropdown-with-img/js/script.js
@@ -19,3 +19,26 @@ $(document).ready(function () {
 		console.log($(this).closest('.dropdown').data('value'));
 	});
 });
+
+
+
+$(document).ready(function () {
+	$('.dropdown-toggle').on('click', function () {
+		$(this).siblings('.dropdown-menu').toggleClass('active');
+		if ($(this).hasClass("active")) {
+			$(this).removeClass("active");
+		} else {
+			$(this).addClass("active");
+		}
+	});
+	$('.dropdown .dropdown-item').on('click', function () {
+		var html = $(this).html();
+		var value = $(this).find('span').data('value');
+		console.log(value);
+		$(this).closest('.dropdown-menu').siblings('.dropdown-toggle').find('.dropdown-toggle-block').html(html);
+		$(this).closest('.dropdown-menu').removeClass('active');
+		$(this).closest('.dropdown-menu').prev().removeClass('active');
+		$(this).closest('.dropdown').data('value', value);
+		console.log($(this).closest('.dropdown').data('value'));
+	});
+});
