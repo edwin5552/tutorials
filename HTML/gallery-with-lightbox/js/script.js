df8ceb38bbfcf2019c46943834e410510859f5d8
Edwin <edwin@encome.in>
2024-07-25 11:22:51 +0530

    Create script.js

---
+++ HTML/gallery-with-lightbox/js/script.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function () {
+
+	//CONSOLE LOG
+	console.log('jQuery Enable');
+
+	//STICKY HEADER
+	jQuery(window).scroll(function () {
+		var scroll = jQuery(window).scrollTop();
+		if (scroll >= 1) {
+			jQuery(".site-header").addClass("scrolled");
+		} else {
+			jQuery(".site-header").removeClass("scrolled");
+		}
+	});
+
+});
+
+// GALLERY LIGHTBOX
+const lightboxGalleryGrid = GLightbox({
+	selector: ".row-gallery-grid a",
+	touchNavigation: true,
+	loop: true,
+	autoplayVideos: true,
+	zoomable: true,
+	draggable: true,
+	preload: true,
+});
